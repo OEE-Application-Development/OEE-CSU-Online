33f4d267653845d666a36383e2cdcc033b643225
Kyle Winterrowd <winterrk@colostate.edu>
2024-12-04 12:40:32 -0700

    Data required for scratch org added

--- bulk/loadAll.bat
+++ bulk/loadAll.bat
@@ -7,3 +7,24 @@ call sf apex run --file bulk/recordtypes/setInstitutionRecordType.apex
 
 call sf data import bulk -s Account --file "bulk/data/CollegeAccounts.csv" -w 10 --column-delimiter=COMMA
 call sf apex run --file bulk/recordtypes/setCollegeRecordType.apex
+
+call sf data import bulk -s Account --file "bulk/data/DepartmentAccounts.csv" -w 10 --column-delimiter=COMMA
+call sf apex run --file bulk/recordtypes/setDepartmentRecordType.apex
+
+REM Banner Account/Contact Pull -- Get Accounts / Link Contacts / Re-update primary Contact on Account
+call sf data import bulk -s Account --file "bulk/data/BannerAccounts.csv" -w 10 --column-delimiter=COMMA
+call sf apex run --file bulk/recordtypes/setBannerAccountRecordType.apex
+call sf data import bulk -s Contact --file "bulk/data/BannerContacts.csv" -w 10 --column-delimiter=COMMA
+call sf data upsert bulk -s Account -i hed__School_Code__c --file "bulk/data/BannerAccountsPrimaryContact.csv" -w 10 --column-delimiter=COMMA
+
+REM EDA/Canvas Objects Terms/Courses/Course Offerings/Enrollments
+call sf data import bulk -s hed__Term__c --file "bulk/data/CreditTerms.csv" -w 10 --column-delimiter=COMMA
+call sf apex run --file bulk/recordtypes/setCreditTermRecordType.apex
+call sf data import bulk -s hed__Course__c --file "bulk/data/CreditCourses.csv" -w 10 --column-delimiter=COMMA
+call sf apex run --file bulk/recordtypes/setCreditCourseRecordType.apex
+call sf data import bulk -s hed__Course_Offering__c --file "bulk/data/CreditSections.csv" -w 10 --column-delimiter=COMMA
+call sf data import bulk -s hed__Course_Enrollment__c --file "bulk/data/CreditStudentEnrollments.csv" -w 10 --column-delimiter=COMMA
+call sf apex run --file bulk/recordtypes/setStudentEnrollmentRecordType.apex
+
+REM Re-Enabling TDTM triggers
+start "Enable TDTM" /wait /b cci task run restore_tdtm_trigger_handlers --namespace hed
